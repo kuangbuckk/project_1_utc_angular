fd914c222c38ab60b1f0fc700aaced84696b28bd
kuangbuckk <kuangbuckk159@gmail.com>
2024-11-05 15:53:02 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { DetailEventComponent } from './detail-event/detail-event.component';
+import { TicketOrderComponent } from './ticket-order/ticket-order.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
-import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    DetailEventComponent,
+    TicketOrderComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    DetailEventComponent
+  ]
 })
 export class AppModule { }
---
+++ src/app/ticket-order/ticket-order.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ticket-order',
+  templateUrl: './ticket-order.component.html',
+  styleUrls: ['./ticket-order.component.scss']
+})
+export class TicketOrderComponent {
+
+}
