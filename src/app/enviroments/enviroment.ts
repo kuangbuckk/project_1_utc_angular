3241030656f720b8d9689ae0766569eebe82ce01
kuangbuckk <kuangbuckk159@gmail.com>
2024-12-02 00:30:42 +0700

    add url from backend

--- src/app/enviroments/enviroment.ts
+++ src/app/enviroments/enviroment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
-    apiBaseUrl: 'http://localhost:8090/api/v1',
+    // apiBaseUrl: 'http://localhost:8090/api/v1',
+    apiBaseUrl: 'https://ticketbooking-deployment.onrender.com/api/v1',
 }
